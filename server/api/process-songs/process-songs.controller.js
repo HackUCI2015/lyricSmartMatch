3419cabdafc0ed06302783c441fb35133b578d52
Max <maxpaulus43@gmail.com>
2015-11-21 05:14:35 -0800

    added watson code and lyric fetch code

--- server/api/process-songs/process-songs.controller.js
+++ server/api/process-songs/process-songs.controller.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var fs = require('fs');
+var watson = require('watson-developer-cloud');
+var lyr = require('lyrics-fetcher');
 
 function handleError (res, err) {
   return res.status(500).send(err);
@@ -13,10 +15,38 @@ function handleError (res, err) {
  * @param req
  * @param res
  */
-exports.index = function (req, res) {
-  fs.readFile('server/api/process-songs/process-songs.data.json', 'utf-8', function (err, processSongss) {
-    if (err) { return handleError(res, err); }
-    res.status(200).json(JSON.parse(processSongss));
+ exports.index = function (req, res) {
+  lyr.fetch('drake', 'started from the bottom', function (err, lyrics) {
+    if (err !== null) {
+      console.log(err);
+    }
+    else {
+      console.log("found lyrics");
+      var personality_insights = watson.personality_insights({
+        username: "60f616bf-9bd5-43d4-988f-be838c2fbbb1",
+        password: "7L8dPygbFFce",
+        version: 'v2'
+      });
+
+      personality_insights.profile({
+        text: lyrics
+      }, function (err, response) {
+        if (err) {
+          console.log('error:', err);
+        }
+        else {
+          var bigFive = response.tree.children[0].children[0].children;
+          for (var i = 0; i < bigFive.length; ++i) {
+            var category = bigFive[i];
+            console.log(category.name + ": " + category.percentage.toFixed(2));
+            for (var j = 0; j < category.children.length; ++j) {
+              var subCategory = category.children[j];
+              console.log('  ' + subCategory.name + ": " + subCategory.percentage.toFixed(2));
+            }
+          }
+        }
+      });
+    }
   });
 };
 
@@ -27,7 +57,7 @@ exports.index = function (req, res) {
  * @param req
  * @param res
  */
-exports.show = function (req, res) {
+ exports.show = function (req, res) {
   res.status(200).json({});
 };
 
@@ -38,7 +68,7 @@ exports.show = function (req, res) {
  * @param req
  * @param res
  */
-exports.create = function (req, res) {
+ exports.create = function (req, res) {
   res.status(201).json({});
 };
 
@@ -49,7 +79,7 @@ exports.create = function (req, res) {
  * @param req
  * @param res
  */
-exports.update = function (req, res) {
+ exports.update = function (req, res) {
   res.status(200).json({});
 };
 
@@ -60,6 +90,6 @@ exports.update = function (req, res) {
  * @param req
  * @param res
  */
-exports.destroy = function (req, res) {
+ exports.destroy = function (req, res) {
   return res.status(204);
 };
