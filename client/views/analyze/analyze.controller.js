a1028970178932c28054a1681c929cf78c6e5c4c
Van Nguyen <vnguyen94@gmail.com>
2015-11-21 08:44:28 -0800

    translate lyric properties to SQL columns

--- client/views/analyze/analyze.controller.js
+++ client/views/analyze/analyze.controller.js
@@ -14,15 +14,14 @@ angular.module('hackuci2015')
         for (var i = 0; i < songList.length; ++i) {
           song = songList[i].split(' - ');
 
-          if (song)
+          if (song) {
             compiledSongList.push({
               artist: song[0],
               name: song[1]
             });
+          }
         }
 
-        console.log('compiledSongList');
-        console.log(compiledSongList);
         $http({
           method: 'GET',
           url: '/api/process-songs',
@@ -32,9 +31,10 @@ angular.module('hackuci2015')
         }).then(function success (data) {
           console.log(data);
         }, function error (err) {
-
-        })
+          console.log(err);
+        });
       }
+
     });
 
   });
--- server/api/process-songs/process-songs.controller.js
+++ server/api/process-songs/process-songs.controller.js
@@ -5,6 +5,44 @@ var watson = require('watson-developer-cloud');
 var lyr = require('lyrics-fetcher');
 var async = require('async');
 
+var translateCategoriesToSQLColumns = {
+  'Openness': 'openness',
+  'Adventurousness': 'adventurousness',
+  'Artistic interests': 'artistic_interests',
+  'Emotionality': 'emotionality',
+  'Imagination': 'imagination',
+  'Intellect': 'intellect',
+  'Authority-challenging': 'authority_challenging',
+  'Conscientiousness': 'conscientiousness',
+  'Achievement striving': 'achievement_striving',
+  'Cautiousness': 'cautiousness',
+  'Dutifulness': 'dutifulness',
+  'Orderliness': 'orderliness',
+  'Self-discipline': 'self_discipline',
+  'Self-efficacy': 'self_efficacy',
+  'Extraversion': 'extraversion',
+  'Activity level': 'activity_level',
+  'Assertiveness': 'assertiveness',
+  'Cheerfulness': 'cheerfulness',
+  'Excitement-seeking': 'excitement_seeking',
+  'Outgoing': 'outgoing',
+  'Gregariousness': 'gregariousness',
+  'Agreeableness': 'agreeableness',
+  'Altruism': 'altruism',
+  'Cooperation': 'cooperation',
+  'Modesty': 'modesty',
+  'Uncompromising': 'uncompromising',
+  'Sympathy': 'sympathy',
+  'Trust': 'trust',
+  'Emotional range': 'emotional_range',
+  'Fiery': 'fiery',
+  'Prone to worry': 'prone_to_worry',
+  'Melancholy': 'melancholy',
+  'Immoderation': 'immoderation',
+  'Self-consciousness': 'self_consciousness',
+  'Susceptible to stress': 'susceptible_to_stress'
+}
+
 function handleError (res, err) {
   return res.status(500).send(err);
 }
@@ -30,11 +68,11 @@ function processCombinedLyrics(err, response) {
 
     for (var i = 0; i < bigFive.length; ++i) {
       var category = bigFive[i];
-      personalityInsight[category.name] = category.percentage.toFixed(2);
+      personalityInsight[translateCategoriesToSQLColumns[category.name]] = category.percentage.toFixed(2);
 
       for (var j = 0; j < category.children.length; ++j) {
         var subCategory = category.children[j];
-        personalityInsight[subCategory.name] = subCategory.percentage.toFixed(2);
+        personalityInsight[translateCategoriesToSQLColumns[subCategory.name]] = subCategory.percentage.toFixed(2);
       }
     }
 
