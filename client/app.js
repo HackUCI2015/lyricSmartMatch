19208951cc6550b4cf8c702fe2f37a0217ba8176
Van Nguyen <vnguyen94@gmail.com>
2015-11-21 00:46:30 -0800

    first commit

---
+++ client/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('hackuci2015', [
+  'ngRoute',
+  'ngAnimate'
+])
+  .config(function ($routeProvider, $locationProvider) {
+
+    $routeProvider
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.html5Mode(true);
+
+  });
---
+++ client/views/home/home.controller.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('hackuci2015')
+  .controller('HomeCtrl', function () {
+
+    var vm = this;
+
+    angular.extend(vm, {
+      name: 'HomeCtrl'
+    });
+
+  });
